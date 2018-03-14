ceca5843627c2b0b2796bb2f18dd599108c1e742
apoc64 <sschwedt@gmail.com>
2018-03-14 17:16:18 -0600

    shorter, better, added parameters to initialize/generate

--- mastermind.rb
+++ mastermind.rb
@@ -2,10 +2,13 @@
 # TDD, GIT...???
 require './game'
 
-puts "Welcome to MASTERMIND\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
+#loop?
+puts "Welcome to MASTERMIND\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?" #ask about difficulty
 user_choice = gets.chomp
 if user_choice == "p"
-  game = Game.new
+  puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game./n" #code to set based on difficulty
+
+  game = Game.new(4, ["R", "G", "B", "Y"])
 elsif user_choice == "i"
   puts "Just play"
 end
