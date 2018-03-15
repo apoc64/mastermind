d8a2c5df46f56a60cf6cbeaf90bdb7f733497cae
apoc64 <sschwedt@gmail.com>
2018-03-15 15:41:03 -0600

    moved ui out of game class

--- game.rb
+++ game.rb
@@ -1,5 +1,7 @@
 #Class for Mastermind game
 class Game
+  attr_reader :answer,
+              :length
 
   def initialize(length, colors)
     @length = length
@@ -7,9 +9,8 @@ class Game
     @number_of_guesses = 0
     @start_time = Time.new
     # puts @answer
-
-    puts "What's your guess?"
-    user_loop
+    puts "    What's your guess?"
+    # user_loop
   end #initialize
 
   def generate_answer(length, char_set) #class?
@@ -18,62 +19,38 @@ class Game
     end.join
   end #generate answer
 
-  def user_loop
-    loop do
-      user_guess = gets.chomp.upcase
-      if user_guess == "Q" #case when then 
-        break
-      elsif user_guess == "C"
-        puts @answer
-      elsif user_guess.length < 4
-        puts "Too short. Guess again."
-      elsif user_guess.length > 4
-        puts "Too long. Guess again."
-      elsif user_guess == @answer
-        you_win
-        break
-      else
-        evaluate_guess(user_guess)
-      end #if user_guess
-    end #loop do
-  end #user_loop
-
   def you_win
     game_time = (Time.new - @start_time).to_i #minutes - /60 %60?
     minutes = game_time/60
     seconds = game_time%60
     puts "Congratulations! You guessed the sequence '#{@answer}' in #{@number_of_guesses} guesses over #{minutes} minutes, #{seconds} seconds"
-    puts "Do you want to (p)lay again or (q)uit?"
-    if gets.chomp == "p"
-      game = Game.new(4, ["R", "G", "B", "Y"]) #go to Mastermind class???
-    end
   end #you_win
 
   def evaluate_guess(guess)
     elements = evaluate_elements(guess)
     positions = evaluate_positions(guess)
     puts "'#{guess}' has #{elements} of the correct elements with #{positions} in the correct positions."
-    puts "Guess again."
+    puts "    Guess again."
     @number_of_guesses += 1
   end #evaluate_guess
 
   def evaluate_elements(guess)
     elements = 0
-    correct_elements = "" #avoid duplication
-    @length.times do |index|
-      guess_char = guess.upcase[index]
+    correct_elements = "" #avoid duplication ?uniq
+    @length.times do |index| #.split
+      guess_char = guess[index]
       if @answer.include?(guess_char) && !correct_elements.include?(guess_char)
         elements += 1
-        correct_elements += guess_char
+        correct_elements << guess_char
       end # a.include
     end
     elements
   end
 
-  def evaluate_positions(guess)
+  def evaluate_positions(guess) #.zip?
     positions = 0
     @length.times do |index|
-      if @answer[index] == guess.upcase[index]
+      if @answer[index] == guess[index]
         positions += 1
       end #if a = g
     end
--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,45 @@
 # Mastermind
 # TDD, GIT...???
 require './game'
+# require 'pry'
 
-#loop?
-puts "Welcome to MASTERMIND\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?" #ask about difficulty
+def user_loop(game)
+  loop do
+    user_guess = gets.chomp.upcase
+    if user_guess == "Q" #case when then
+      break
+    elsif user_guess == "C"
+      puts game.answer
+    elsif user_guess.length < game.length
+      puts "Too short. Guess again."
+    elsif user_guess.length > game.length
+      puts "Too long. Guess again."
+    elsif user_guess == game.answer
+      game.you_win
+      game_won
+      break
+    else
+      game.evaluate_guess(user_guess)
+    end #if user_guess
+  end #loop do
+end #user_loop
+
+def game_won
+  puts "\nDo you want to (p)lay again or (q)uit?"
+  if gets.chomp == "p"
+    game = Game.new(4, ["R", "G", "B", "Y"])
+    user_loop(game)
+  end
+end
+
+# Start of game
+puts "\n   *** Welcome to MASTERMIND ***\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?" #ask about difficulty
 user_choice = gets.chomp
-if user_choice == "p"
-  puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game./n" #code to set based on difficulty
 
+if user_choice == "p"
+  puts "\nI have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.\n\n" #code to set based on difficulty
   game = Game.new(4, ["R", "G", "B", "Y"])
+  user_loop(game)
 elsif user_choice == "i"
   puts "Just play"
 end
